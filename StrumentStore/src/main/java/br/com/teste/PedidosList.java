eb250d4479a4afc979844d6565f584c68f9f66c4
Italo Alcântara <italoalcantara6@gmail.com>
2024-10-05 16:56:15 -0300

    Delete StrumentStore directory

--- StrumentStore/src/main/java/br/com/teste/PedidosList.java
+++
@@ -1,9 +0,0 @@
-package br.com.teste;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class PedidosList {
-	public static List<Pedidos> pedidosList = new ArrayList<Pedidos>();
-	
-}
